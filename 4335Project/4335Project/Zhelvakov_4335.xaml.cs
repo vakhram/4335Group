59a79cefd14d329cdccdf52375c0e887a46feff2
a.zhelvakov <a.zhelvakov@otus.ru>
2025-01-22 10:54:47 +0300

    Zhelvakov

---
+++ 4335Project/4335Project/Zhelvakov_4335.xaml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _4335Project
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Zhelvakov Arseniy");
+        }
+    }
+}
